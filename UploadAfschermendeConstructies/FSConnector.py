fd3642b49ce56c6eff7b2c6902dda8601a08dc66
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-26 12:59:55 +0100

    FS connector limit dependency juist zetten

--- UploadAfschermendeConstructies/FSConnector.py
+++ UploadAfschermendeConstructies/FSConnector.py
@@ -2,10 +2,12 @@ import requests
 
 
 class FSConnector:
-    def get_raw_lines(self, layer, lines):
+    def get_raw_lines(self, layer, lines: int=-1):
         cert_path = 'C:\\resources\\datamanager_eminfra_prd.awv.vlaanderen.be.crt'
         key_path = 'C:\\resources\\datamanager_eminfra_prd.awv.vlaanderen.be.key'
-        url = f'https://services.apps.mow.vlaanderen.be/geolatte-nosqlfs/cert/api/databases/featureserver/{layer}/query?limit={lines}' # TODO query weglatn indien lines is None
+        url = f'https://services.apps.mow.vlaanderen.be/geolatte-nosqlfs/cert/api/databases/featureserver/{layer}/query'
+        if lines > -1:
+            url += f'?limit={lines}'
         response = requests.get(url, cert=(cert_path, key_path))
         if response.status_code == 200:
 
